47ff4b7e31303b23eea9c06e78eecb3de6ed639a
mohamedrashad102 <mmm682552@gmail.com>
2024-07-20 14:18:40 +0300

    Delete avg.dart and sum.dart
    The commit message indicates that the files avg.dart and sum.dart were deleted.

--- avg.dart
+++
@@ -1,7 +0,0 @@
-double avg(List<double> numbers) {
-  double sum = 0;
-  numbers.forEach((num) {
-    sum += num;
-  });
-  return sum / numbers.length;
-}
--- sum.dart
+++
@@ -1,7 +0,0 @@
-int sum(List<int> list) {
-  int sum = 0;
-  for (int i = 0; i < list.length; i++) {
-    sum += list[i];
-  }
-  return sum;
-}
